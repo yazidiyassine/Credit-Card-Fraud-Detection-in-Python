c0c5868d4200814ddcd58de494ffb39f43acd0a7
Yassine Yazidi <104203113+yazidiyassine@users.noreply.github.com>
2023-04-23 22:49:59 +0100

    Step 2: Data Preparation

---
+++ README.md
@@ -0,0 +1,31 @@
+# Project Title: Credit Card Fraud Detection in Python
+This is a project that aims to detect fraudulent credit card transactions using Python. The dataset used in this project is obtained from Kaggle's Credit Card Fraud Detection dataset.
+
+## Project Description
+The project consists of three main parts:
+
+### Data exploration and visualization
+This part involves analyzing and visualizing the data to understand the features and identify any patterns or anomalies.
+
+### Data preprocessing
+This part involves preparing the data for the machine learning algorithms by handling missing values, scaling the features, and splitting the data into training and testing sets.
+
+### Machine learning
+This part involves building and training machine learning models to predict fraudulent transactions. The machine learning models used in this project are:
+
+- Logistic Regression
+- Random Forest Classifier
+
+## Requirements
+The project requires the following Python packages:
+
+- pandas
+- numpy
+- matplotlib
+- seaborn
+- scikit-learn
+
+These packages can be installed using pip.
+
+## Credits
+The dataset used in this project is obtained from Kaggle's Credit Card Fraud Detection dataset.
---
+++ ccfd_data_preparation.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 11:19:52 2023
+
+@author: Yassine Yazidi
+"""
+
+# Importing modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import gridspec
+
+
+dataset = pd.read_csv("creditcard.csv")
+
+# =============================================================================
+# Data Preparation
+# =============================================================================
+
+# Check for missing|null values
+dataset.isnull().shape[0]
+print("None-missing values: "+str(dataset.isnull().shape[0]))
+print("Misssing values: "+ str(dataset.shape[0] - dataset.isnull().shape[0]))
+
+""" As there are no missing data, we turn to standardization.
+We standardize only Time and Amount using RobustScaler:
+ """
+from sklearn.preprocessing import RobustScaler
+""" This Scaler removes the median and scales the data
+according to the quantile range (defaults to IQR:
+Interquartile Range). The IQR is the range between the 1st quartile (25th quantile) and the 3rd quartile (75th quantile). """
+scaler = RobustScaler().fit(dataset[["Time", "Amount"]])
+dataset[["Time", "Amount"]] = scaler.transform(dataset[["Time", "Amount"]])
+
+print(dataset.head().append(dataset.tail())) # type: ignore
+
+"""Dividing the data into features and targets.
+Making the train-test split of the data: """
+
+# Separate response and features underSampling before cross validation will lead to overfitting 
+y = dataset["Class"] # target
+x = dataset.iloc[:, 0:30]
+
+# Use sklearn for splitting the data
+from sklearn.model_selection import train_test_split
+X_train, x_test, Y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+print(X_train.shape, x_test.shape, Y_train.shape, y_test.shape)
